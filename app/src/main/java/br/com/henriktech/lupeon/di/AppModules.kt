12c79643a1557177720e1b507df734cc469127a2
Ivam Henrique <ivam.henrique@outlook.com>
2021-08-30 15:16:05 -0300

    Correção de Injectção de dependencias e Databinding

--- app/src/main/java/br/com/henriktech/lupeon/di/AppModules.kt
+++ app/src/main/java/br/com/henriktech/lupeon/di/AppModules.kt
@@ -4,19 +4,21 @@ import org.koin.core.context.loadKoinModules
 
 object AppModules {
     fun loadModule() {
-        loadKoinModules(listOf(
-            SplashModule.get(),
-            LoginPresentationModule.get(),
-            LoginMainModule.get(),
-            LoginLossPasswordModule.get(),
-            LoginNewPasswordModule.get(),
-            DriverModule.get(),
-            ProfileAllowanceModule,
-            ProfileIndicatorsModule,
-            ProfileMenuModule.get(),
-            ProfileSimulationModule.get(),
-            ProfileTokenModule.get(),
-            ProfileTrackingModule.get()
-        ))
+        loadKoinModules(
+            listOf(
+                SplashModule.get(),
+                LoginPresentationModule.get(),
+                LoginMainModule.get(),
+                LoginLossPasswordModule.get(),
+                LoginNewPasswordModule.get(),
+                DriverModule.get(),
+                ProfileAllowanceModule.get(),
+                ProfileIndicatorsModule.get(),
+                ProfileMenuModule.get(),
+                ProfileSimulationModule.get(),
+                ProfileTokenModule.get(),
+                ProfileTrackingModule.get()
+            )
+        )
     }
 }
