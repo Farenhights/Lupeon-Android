014d8279b385d10325f81a1c0160acf39967c993
Ivam Henrique <ivam.henrique@outlook.com>
2022-03-24 21:43:53 -0300

    build: view de item de lista

---
+++ app/src/main/java/br/com/henriktech/lupeon/ui/invoice/InvoiceFragment.kt
@@ -0,0 +1,4 @@
+package br.com.henriktech.lupeon.ui.invoice
+
+class InvoiceFragment {
+}
--- app/src/main/java/br/com/henriktech/lupeon/ui/tracking/occurrence/OcccurrenceFragment.kt
+++ app/src/main/java/br/com/henriktech/lupeon/ui/tracking/occurrence/OcccurrenceFragment.kt
@@ -12,21 +12,19 @@ class OcorrencyFragment : Fragment(R.layout.fragment_tracking_occurrence) {
     private val analytics: OccurrenceAnalytics by inject()
     private val viewModel: OccurrenceViewModel by viewModel()
 
-    private lateinit var binding: FragmentTrackingOccurrenceBinding
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         analytics.trackScreen(requireActivity())
-        binding = FragmentTrackingOccurrenceBinding.bind(view)
+        val binding = FragmentTrackingOccurrenceBinding.bind(view)
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
         startView(binding)
-        startViewModel()
+        startViewModel(binding)
     }
 
     private fun startView(binding: FragmentTrackingOccurrenceBinding) {
     }
 
-    private fun startViewModel() {
+    private fun startViewModel(binding: FragmentTrackingOccurrenceBinding) {
     }
 }
