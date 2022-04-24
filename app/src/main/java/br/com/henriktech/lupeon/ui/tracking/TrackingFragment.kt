39bf9d3de4789d6d8ca3379ea0fbe8327d61c278
Ivam Henrique <ivam.henrique@outlook.com>
2022-04-24 19:40:19 -0300

    build: fluxo embarcador e transportador separados no tracking

--- app/src/main/java/br/com/henriktech/lupeon/ui/tracking/TrackingFragment.kt
+++ app/src/main/java/br/com/henriktech/lupeon/ui/tracking/TrackingFragment.kt
@@ -46,7 +46,7 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         binding.trackingSearchButton.setOnClickListener {
             val invoice = Integer.parseInt(binding.textNumberInvoice.text.toString())
             saveInvoice(invoice, binding.spinnerFilial.selectedItemPosition)
-            findNavController().navigate(R.id.action_trackingFragment_to_invoiceFragment)
+            findNavController().navigate(R.id.action_trackingFragment_to_deliveryFragment)
         }
 
     }
